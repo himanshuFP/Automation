92457f0a228c316eb2a751b8f4968ab4984948c5
Patrick <himanshunewheights@gmail.com>
2026-07-18 19:40:37 +0530

    test file pom xml

---
+++ src/test/java/tests/GoogleTest.java
@@ -0,0 +1,31 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class GoogleTest {
+
+    @Test
+    public void verifyGoogleTitle() {
+
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://www.google.com");
+
+        Assert.assertEquals(driver.getTitle(), "Google");
+
+        driver.quit();
+    }
+}
